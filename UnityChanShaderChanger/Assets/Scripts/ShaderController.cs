c0d3d5f481bc832ed13157e5181a3a68e4971fe7
TY-A-02-173\akira.sugisaki <akira.sugisaki@linkedbrain.jp>
2025-03-21 19:04:46 +0900

    update materialではなくshaderで指定するように、materialが複数ついているオブジェクトもあったので指定の仕方を修正 todo Unlitだと目しか表示されなくなる

--- UnityChanShaderChanger/Assets/Scripts/ShaderController.cs
+++ UnityChanShaderChanger/Assets/Scripts/ShaderController.cs
@@ -3,20 +3,15 @@ using UnityEngine;
 public class ShaderController : MonoBehaviour
 {
     public GameObject avatarModel; // アバターのモデル
-    // public Shader[] shaders; // 使用するシェーダーの配列
-    public Material[] materials; // 使用するマテリアルの配列
+    public Shader[] shaders; // 使用するシェーダーの配列
 
     private int currentShaderIndex = 0; // 現在のシェーダーインデックス
 
     // シェーダーを変更
     public void ChangeShader()
     {
-        // currentShaderIndex = (currentShaderIndex + 1) % shaders.Length; // 次のシェーダーにインデックスを更新
-        // avatarModel.GetComponent<Renderer>().material.shader = shaders[currentShaderIndex]; // 現在のシェーダーを適用
-        currentShaderIndex = (currentShaderIndex + 1) % materials.Length; // 次のシェーダーにインデックスを更新
-        Debug.Log(materials[currentShaderIndex]);
-
-        // avatarModel.GetComponent<Renderer>().material.shader = materials[currentShaderIndex].shader; // 現在のシェーダーを適用
+        currentShaderIndex = (currentShaderIndex + 1) % shaders.Length; // 次のシェーダーにインデックスを更新
+        Debug.Log(shaders[currentShaderIndex]);
 
         // avatarModel内の全ての SkinnedMeshRenderer を取得
         SkinnedMeshRenderer[] renderers = avatarModel.GetComponentsInChildren<SkinnedMeshRenderer>();
@@ -26,7 +21,10 @@ public class ShaderController : MonoBehaviour
         {
             if (renderer != null)
             {
-                renderer.material.shader = materials[currentShaderIndex].shader; // マテリアルを適用
+                foreach (var material in renderer.materials)
+                {
+                    material.shader = shaders[currentShaderIndex];
+                }
             }
         }
     }
