dc138f350ebb6f55ba99165a717a4e1303c6fee0
TY-A-02-173\akira.sugisaki <akira.sugisaki@linkedbrain.jp>
2025-03-21 19:57:38 +0900

    add 選択中のシェーダーを表示するテキストを追加

--- UnityChanShaderChanger/Assets/Scripts/ShaderController.cs
+++ UnityChanShaderChanger/Assets/Scripts/ShaderController.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class ShaderController : MonoBehaviour
 {
     public GameObject avatarModel; // アバターのモデル
     public Shader[] shaders; // 使用するシェーダーの配列
+    public TextMeshProUGUI shaderName;  // 選択中のシェーダーを表示するテキスト
 
     private int currentShaderIndex = 0; // 現在のシェーダーインデックス
+
+    private void Start()
+    {
+        shaderName.text = shaders[currentShaderIndex].name;
+    }
 
     // シェーダーを変更
     public void ChangeShader()
     {
         currentShaderIndex = (currentShaderIndex + 1) % shaders.Length; // 次のシェーダーにインデックスを更新
-        Debug.Log(shaders[currentShaderIndex]);
+        shaderName.text = shaders[currentShaderIndex].name;
 
         // avatarModel内の全ての SkinnedMeshRenderer を取得
         SkinnedMeshRenderer[] renderers = avatarModel.GetComponentsInChildren<SkinnedMeshRenderer>();
